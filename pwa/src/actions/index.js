2261bf87b07976b31ab8464e4fa3850bb7a0ffd3
AntipovAndrey <antipov11.11@gmail.com>
2019-02-09 15:35:08 +0300

    [pwa] add todo action creator

--- pwa/src/actions/index.js
+++ pwa/src/actions/index.js
@@ -1,6 +1,15 @@
+import uuid from 'uuid';
 import {ADD_TODO, TOGGLE_TODO, REMOVE_TODO} from './types';
 
-export const addTodo = todo => ({type: ADD_TODO, payload: todo});
+export const addTodo = todo => ({
+  type: ADD_TODO,
+  payload: {
+    id: uuid(),
+    date: new Date(),
+    title: todo,
+    completed: false
+  }
+});
 
 export const toggleTodo = todoId => ({type: TOGGLE_TODO, payload: todoId});
 
