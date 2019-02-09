ed4535af72f8febe185945e3b2f0c3f21d078764
AntipovAndrey <antipov11.11@gmail.com>
2019-02-09 14:24:38 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pwa/precache-manifest.6e9c1d0136f0c29de611f0d55d2254a8.js"
+  "/PWA/precache-manifest.ac83cb2166fe699ddbf824ba20d2402c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/pwa/index.html", {
+workbox.routing.registerNavigationRoute("/PWA/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
