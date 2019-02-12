7442bd19d16cd5a55d47594fd3fddc5d58b8dd8a
AntipovAndrey <antipov11.11@gmail.com>
2019-02-13 00:01:06 +0300

    [pwa] delete button

--- pwa/src/components/TodoListItem.js
+++ pwa/src/components/TodoListItem.js
@@ -3,22 +3,36 @@ import {withStyles} from '@material-ui/core/styles';
 import ListItem from '@material-ui/core/ListItem';
 import Checkbox from '@material-ui/core/Checkbox';
 import Typography from '@material-ui/core/Typography';
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from '@material-ui/core/IconButton';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 
 const styles = theme => ({
   completed: {
+
     textDecorationLine: 'line-through',
   },
+  trash: {
+    marginRight: 10,
+  }
 });
 
 const TodoListItem = ({todo, onToggled, onRemoved, classes}) => (
-  <ListItem key={todo.id} button onClick={onToggled}>
+  <ListItem key={todo.id} button dense onClick={onToggled}>
     <Checkbox
       checked={todo.completed}
-      tabIndex={-1}
-      disableRipple/>
-    <Typography className={todo.completed ? classes.completed : null} variant="subheading" color="inherit">
+      tabIndex={-1}/>
+    <Typography
+      className={todo.completed ? classes.completed : null}
+      color={todo.completed ? "textSecondary" : "inherit"}
+      variant="subtitle1">
       {todo.title}
     </Typography>
+    <ListItemSecondaryAction>
+      <IconButton onClick={() => setTimeout(onRemoved, 250)} className={classes.trash} aria-label="Delete">
+        <DeleteIcon/>
+      </IconButton>
+    </ListItemSecondaryAction>
   </ListItem>
 );
 
