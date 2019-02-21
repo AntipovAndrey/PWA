267d5c27dee62f2883b814698026b2260b290b72
AntipovAndrey <antipov11.11@gmail.com>
2019-02-21 18:56:05 +0300

    [android] url with query params

--- android/app/src/main/java/ru/andrey/pwa/MainActivity.kt
+++ android/app/src/main/java/ru/andrey/pwa/MainActivity.kt
@@ -1,10 +1,12 @@
 package ru.andrey.pwa
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.customtabs.trusted.LauncherActivity
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.net.URI
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,7 +14,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         open_twa_button.setOnClickListener {
-            startActivity(Intent(this, LauncherActivity::class.java))
+            startActivity(createIntent())
         }
+    }
+
+    private fun createIntent(): Intent {
+        var url = BuildConfig.DEFAULT_URL
+
+        with(todo_text.text.toString()) {
+            if (isNotBlank()) {
+                url = appendQuery(url, "todo=${this}")
+            }
+        }
+
+        return Intent(this, LauncherActivity::class.java).apply { data = Uri.parse(url) }
+    }
+
+    private fun appendQuery(uri: String, appendQuery: String): String {
+        val oldUri = URI(uri)
+        return URI(oldUri.scheme, oldUri.authority, oldUri.path, appendQuery, oldUri.fragment).toString()
     }
 }
